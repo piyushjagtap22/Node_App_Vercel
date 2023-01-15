e30c2ac8715209b16e0b6ee4ccdba07530ad2a48
piyushjagtap22 <jagspiyush@gmail.com>
2023-01-15 18:25:07 +0530

    enabling cors

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // index.js
 const express = require('express')
 
+const cors = require('cors');
+
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
 
 const connectDB = require('./config/db');
 const path = require('path');
